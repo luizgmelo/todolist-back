3a369f9d4d610ec9e4f661c7753a9231e55bf83a
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2025-04-04 16:48:56 -0300

    feat: add some modifiations

--- repository/taskRepository.js
+++ repository/taskRepository.js
@@ -2,64 +2,33 @@ import db from '../config/db.js';
 
 const taskRepository = {
     listAll: (callback) => {
-        db.all('SELECT * FROM tasks', [], callback);
+      db.all('SELECT * FROM tasks', [], callback);
     },
+
+    getTaskById: (id, callback) => {
+      db.get('SELECT * FROM tasks WHERE id = ?', [id], (err, task) => {
+        if (err) {
+          return callback(err, null);
+        }
+        return callback(null, task);
+      });
+    },
+
     create: (title, callback) => {
-        db.run('INSERT INTO tasks (title) VALUES (?)', [title],
-            function(err) {
-                if (err) {
-                    return callback(err, null);
-                }
-                db.get('SELECT * FROM tasks WHERE id = ?', [this.lastID], (err, row) => {
-                    if (err) {
-                        return callback(err, null);
-                    }
-                    return callback(null, row);
-                });
-            }
-        );
+      db.run('INSERT INTO tasks (title) VALUES (?)', [title],
+        function(err) {
+          if (err) {
+            return callback(err, null);
+          }
+          
+        taskRepository.getTaskById(this.lastID, callback);
+        }
+      );
     },
+
     update: (taskData, callback) => {
-
-
-        db.get(
-            'SELECT isCompleted FROM tasks WHERE id = ?', [taskData.id], (err, task) => {
-                if (err) {
-                    callback(err, null);
-                }
-                else if (!task) {
-                    console.log('Task not found!')
-                    callback(null, null)
-                } else {
-
-                    let newStatus;
-
-                    if (taskData.isCompleted === undefined || taskData.isCompleted === null) {
-                        newStatus = task.isCompleted;
-                    }
-
-                    newStatus = taskData.isCompleted ? 1 : 0;
-
-                    db.run('UPDATE FROM tasks SET title=?, "isCompleted"=? WHERE id=?', [taskData.title, newStatus, taskData.id], (err, _) => {
-
-                        if (err) {
-                            callback(err, null)
-                        }
-
-                        db.get('SELECT * FROM tasks WHERE id = ?', [taskData.id], callback)
-                    })
-
-
-                }
-            }
-
-
-        )
-
-
-
-
     },
+
     delete: (id, callback) => {
         db.run(
             'DELETE FROM tasks WHERE id = ?', [id],
